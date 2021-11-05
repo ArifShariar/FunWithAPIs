ed7826abae2edd892c491fc0c93c5e606b173ffb
Arif Shariar Rahman <43639863+ArifShariar@users.noreply.github.com>
2021-11-06 01:15:37 +0600

    integration with database

--- RandomFox/models.py
+++ RandomFox/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Fox(models.Model):
     id_from_web = models.IntegerField()
     link_from_web = models.CharField(max_length=200)
-    image_form_web = models.ImageField()
+    image_form_web = models.ImageField(upload_to='media/foxes/')
     access_time = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- RandomFox/views.py
+++ RandomFox/views.py
@@ -1,5 +1,10 @@
+import os
+import re
+
 from django.shortcuts import render
 import requests
+from .models import Fox
+import cv2
 
 
 # Create your views here.
@@ -7,4 +12,18 @@ def randomFox(request):
     response = requests.get('https://randomfox.ca/floof/')
     fox_images = response.json()
     context = {'images': fox_images['image']}
+    numbers = re.findall('[0-9]+', fox_images['link'])
+    res = requests.get(fox_images['image'])
+    file_name = 'fox' + numbers[0] + '.jpg'
+    with open(file_name, 'wb') as f:
+        f.write(res.content)
+        f.close()
+    fox = Fox()
+    fox.id_from_web = numbers[0]
+    fox.link_from_web = fox_images['link']
+    # ERROR:file name is a string, not a file object
+    fox.image_form_web = file_name
+    fox.save()
+    os.remove(file_name)
+
     return render(request, 'RandomFox/randomFox.html', context)
