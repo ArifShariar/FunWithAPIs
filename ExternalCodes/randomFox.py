1d135835bfb85f12fd682617928185a6dea725fd
Arif Shariar Rahman <43639863+ArifShariar@users.noreply.github.com>
2021-11-05 20:38:47 +0600

    added FOX class and updated some codes

--- ExternalCodes/randomFox.py
+++ ExternalCodes/randomFox.py
@@ -3,11 +3,11 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
-
 response = requests.get('https://randomfox.ca/floof/')
 fox_images = response.json()
 print(fox_images)
 print(fox_images['image'])
 print(fox_images['link'])
 
-
+numbers = re.findall('[0-9]+', fox_images['link'])
+print(int(numbers[0]))
--- RandomFox/models.py
+++ RandomFox/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Fox(models.Model):
+    id_from_web = models.IntegerField()
+    link_from_web = models.CharField(max_length=200)
+    image_form_web = models.ImageField()
+    access_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.link_from_web
