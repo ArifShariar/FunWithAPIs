049589355a9a34965c58c31d0ace61f9c109b9e9
Arif Shariar Rahman <43639863+ArifShariar@users.noreply.github.com>
2021-11-05 21:41:55 +0600

    download image from url and open with opencv complete

--- ExternalCodes/randomFox.py
+++ ExternalCodes/randomFox.py
@@ -1,13 +1,25 @@
-# read an html file and extract all the images
 import re
+import urllib
 import requests
 from bs4 import BeautifulSoup
-
+import cv2
 response = requests.get('https://randomfox.ca/floof/')
 fox_images = response.json()
 print(fox_images)
 print(fox_images['image'])
 print(fox_images['link'])
 
+
+# download images from url
+# and open it using opencv
 numbers = re.findall('[0-9]+', fox_images['link'])
-print(int(numbers[0]))
+res = requests.get(fox_images['image'])
+file_name = 'fox' + numbers[0] + '.jpg'
+with open(file_name, 'wb') as f:
+    f.write(res.content)
+    f.close()
+    print("download complete")
+    img = cv2.imread(file_name)
+    cv2.imshow('image', img)
+    cv2.waitKey(0)
+
