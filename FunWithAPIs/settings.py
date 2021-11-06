c0ce446f9755911ee3c5523285b690ecff6fdabb
Arif Shariar Rahman <43639863+ArifShariar@users.noreply.github.com>
2021-11-06 20:48:34 +0600

    Added weather app

--- FunWithAPIs/settings.py
+++ FunWithAPIs/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'RandomFox',
     'Home',
+    'Weather',
 ]
 
 MIDDLEWARE = [
--- FunWithAPIs/urls.py
+++ FunWithAPIs/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Home.urls')),
     path('randomFox/', include('RandomFox.urls')),
+    path('weather/', include('Weather.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
