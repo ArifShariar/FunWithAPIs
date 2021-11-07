d615bd63fe4127c6e84832587c45df290f0dc44f
Arif Shariar Rahman <43639863+ArifShariar@users.noreply.github.com>
2021-11-07 19:47:26 +0600

    Added autocomplete to input district name

--- Weather/districtExtractor.py
+++
@@ -1,6 +0,0 @@
-import json
-
-# read json file
-with open('files/bd_districts.json', encoding='utf-8') as json_file:
-    data = json.load(json_file)
-    print(data['districts'][2]['name'])
--- Weather/views.py
+++ Weather/views.py
@@ -1,5 +1,16 @@
+import json
+
 from django.shortcuts import render
 
 
 def weather(request):
-    return render(request, 'Weather/weather.html')
+    with open('Weather/files/bd_districts.json', encoding='utf-8') as json_file:
+        data = json.load(json_file)
+
+    districts = []
+    k = 1
+    while k <= 63:
+        districts.append(data['districts'][k]['name'])
+        k += 1
+    context = {'districts': districts}
+    return render(request, 'Weather/weather.html', context)
