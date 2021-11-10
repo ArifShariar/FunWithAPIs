d11717ee22ba8cbe9cac44f8afc61010a7273ab7
Arif Shariar Rahman <43639863+ArifShariar@users.noreply.github.com>
2021-11-10 13:23:08 +0600

    Weather API full complete

--- Weather/views.py
+++ Weather/views.py
@@ -23,8 +23,24 @@ def weather(request):
         apikey = '03c0dcc9761cb8f6c56ef511f958d11d'
         req = 'https://api.openweathermap.org/data/2.5/weather?q=' + district_name + '&units=metric&appid=' + apikey
         response = requests.get(req)
-        print(response.json())
-        return render(request, 'Weather/weather.html', context)
+        if response.status_code == 404:
+            return HttpResponse("<h1>City not available in API</h1>")
+        else:
+            context['lat'] = response.json()['coord']['lat']
+            context['lon'] = response.json()['coord']['lon']
+            context['weather_main'] = response.json()['weather'][0]['main']
+            context['weather_description'] = response.json()['weather'][0]['description']
+            context['temp'] = response.json()['main']['temp']
+            context['temp_feels'] = response.json()['main']['feels_like']
+            context['temp_min'] = response.json()['main']['temp_min']
+            context['temp_max'] = response.json()['main']['temp_max']
+            context['pressure'] = response.json()['main']['pressure']
+            context['humidity'] = response.json()['main']['humidity']
+            context['visibility'] = response.json()['visibility']
+            context['wind_speed'] = response.json()['wind']['speed']
+            context['wind_deg'] = response.json()['wind']['deg']
+            context['clouds'] = response.json()['clouds']['all']
+            return render(request, 'Weather/weather.html', context)
     else:
 
         return render(request, 'Weather/weather.html', context)
