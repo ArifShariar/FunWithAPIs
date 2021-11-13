575f82eb7f2afcdf1fd69759ec81a58d2022a993
Arif Shariar Rahman <43639863+ArifShariar@users.noreply.github.com>
2021-11-13 21:41:10 +0600

    NASA astronomy picture of the day API integration complete

--- NASA/views.py
+++ NASA/views.py
@@ -1,5 +1,14 @@
+import requests
 from django.shortcuts import render
 
 
 def NASA_astronomy_picture_of_the_day(request):
-    return render(request, 'NASA/NASA.html')
+    context = {}
+    req = requests.get('https://api.nasa.gov/planetary/apod?api_key=JbPh57ub5qcB6frjO0UHzsSxiySqwyiZe7b3WpZe')
+    context['copyright'] = req.json()['copyright']
+    context['title'] = req.json()['title']
+    context['date'] = req.json()['date']
+    context['explanation'] = req.json()['explanation']
+    context['url'] = req.json()['url']
+    context['hdurl'] = req.json()['hdurl']
+    return render(request, 'NASA/NASA.html', context)
