5218001e9ff4928d60a261b9bd352c1bb267ea25
Arif Shariar Rahman <43639863+ArifShariar@users.noreply.github.com>
2021-11-13 21:25:22 +0600

    url changed for NSA APOD

--- NASA/urls.py
+++ NASA/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.NASA, name='NASA'),
+    path('astronomy_picture_of_the_day', views.NASA_astronomy_picture_of_the_day,
+         name='NASA_astronomy_picture_of_the_day'),
 ]
--- NASA/views.py
+++ NASA/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 
-def NASA(request):
+def NASA_astronomy_picture_of_the_day(request):
     return render(request, 'NASA/NASA.html')
